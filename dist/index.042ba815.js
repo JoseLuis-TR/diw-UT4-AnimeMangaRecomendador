565d766ce270bafb60225eed03dd829a110768b7
Jose-LTR <jtocroj094@g.educaand.es>
2022-12-04 20:44:18 +0100

    Primer intento de menu desplegable

---
+++ dist/index.042ba815.js
@@ -0,0 +1,9 @@
+const MENU = document.getElementById("menu");
+function abrirMenu() {
+    MENU.style.width = "250px";
+}
+function cerrarMenu() {
+    MENU.style.width = "0";
+}
+
+//# sourceMappingURL=index.042ba815.js.map
---
+++ src/assets/js/menu.js
@@ -0,0 +1,9 @@
+const MENU = document.getElementById("menu");
+
+function abrirMenu(){
+    MENU.style.width = "250px"
+}
+
+function cerrarMenu(){
+    MENU.style.width = "0"
+}
